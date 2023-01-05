d34728ea36a78bc8242edaf0c1037ee24c449956
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2023-01-05 01:50:53 -0500

    Deleting experimental code

--- Python/Display.py
+++ Python/Display.py
@@ -4,12 +4,6 @@ def new_line(num):
         
 def viewer(arr): 
     print('\n'.join([''.join(['{:^{}}'.format(cell, 5) for cell in row]) for row in arr]))    
-   # if not np.where(arr == '\U0001F93A')[0] == '':
-        #player_y = np.where(arr == '\U0001F93A')[1][0]
-        #new_col = np.array(['' for _ in range(arr.shape[1])]).T
-        #new_arr =  np.insert(arr, player_y - 1, new_col, axis=1)
-        #print('\n'.join([''.join(['{:^{}}'.format(cell, 5) for cell in row]) for row in new_arr]))
-    
-    #else:
+
          
     
