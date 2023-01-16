6ff9263ce73e4548ae5a4ca1ce61592e9a35c0b1
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2023-01-16 18:51:34 -0500

    Minor changes and change file structure

---
+++ R/Base R/Scripts/Create_Environment.R
@@ -0,0 +1,14 @@
+#Create local environment
+castle_descent <- new.env(parent = globalenv())
+#List files in folder
+castle_descent_files <- c("Castle_Create.R", "Events.R", "Classes.R",
+           "Inventory.R", "Menu_Cursor.R","Merchant.R", "Display.R","Item_Descriptions.R"
+           ,"Main_Loop.R")
+#source files to local environment
+for(castle_descent_file in castle_descent_files){
+  source(castle_descent_file, local = castle_descent)
+}
+#Save environment
+save(castle_descent,file = "Castle_Descent_Base_R.RData")
+#Remove variables from global environment
+rm(castle_descent_file,castle_descent_files,castle_descent)
