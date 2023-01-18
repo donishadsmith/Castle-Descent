b0d481c32f28913fe5cfea628f808cb796a08c4e
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2023-01-18 15:23:22 -0500

    Delete R directory

--- R/Base R/Create_Text_Files.R
+++
@@ -1,5 +0,0 @@
-#List files in folder
-castle_descent_files <- c("Castle_Create.R", "Events.R", "Classes.R",
-           "Inventory.R", "Menu_Cursor.R","Merchant.R", "Display.R","Item_Descriptions.R"
-           ,"Main_Loop.R")
-write(castle_descent_files, "Castle_Descent_Files.txt")
