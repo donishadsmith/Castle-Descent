ca3754b6d1cd4d5d63b49ae90c41f9084ef8b060
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2023-01-18 22:50:06 -0500

    Add files via upload

---
+++ R/R Studio/Start_Game.R
@@ -0,0 +1,11 @@
+#Create local environment
+castle_descent <- new.env(parent = globalenv())
+#source files to local environment
+for(castle_descent_file in dir()){
+  if(!castle_descent_file == ("Start_Game.R")){
+    source(castle_descent_file, local = castle_descent)
+  }
+}
+#Remove variables from global environment
+rm(castle_descent_file)
+castle_descent$start_game()
