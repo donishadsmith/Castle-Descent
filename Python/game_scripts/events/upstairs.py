2954086a95409488eea43cd47e916e95b539e9a6
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2023-02-18 19:36:12 -0500

    Better file organization

---
+++ Python/game_scripts/events/upstairs.py
@@ -0,0 +1,12 @@
+import time
+from display import *
+               
+def upstairs_event(castle,player):    
+    display_array(castle = castle, game_sequence = 'non-battle', player = player)
+    print('You already came from upstairs.')
+    time.sleep(1)
+
+ 
+ 
+   
+    
